9a56a4d621d9e4498b72b88a4eb910c480365dea
Aura <gutmmm@gmail.com>
2023-06-19 21:19:38 +0200

    Changed layout. Backend to be fixed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QTimer, QUrl, Qt, QEvent)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
-from PySide2.QtWidgets import QGraphicsBlurEffect
 
 from ui_circleTimer import Ui_MainWindow
 import datetime
@@ -17,76 +16,63 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.show()
 
-        # FUNCTIONS CONNECT
-        self.ui.start_button.clicked.connect(self.check_user_input)
-        self.ui.timeEdit.timeChanged.connect(lambda: self.calculate_time())
-
-        self.init_value = 0
-        self.duration = 0
-        self.stepper = 0
-
-        self.blur = QGraphicsBlurEffect()
-        self.blur.setBlurRadius(15)
-
-        self.unblur = QGraphicsBlurEffect()
-        self.unblur.setBlurRadius(0)
-  
-    def calculate_time(self):
-        self.duration = (self.ui.timeEdit.time().hour()*60 + self.ui.timeEdit.time().minute())*60
-        self.step = 1/self.duration
-        self.start_time = str(datetime.timedelta(seconds=self.duration))
-
-
-    def check_user_input(self):
-        if self.ui.plainTextEdit.toPlainText() == '' or self.duration == 0:
-            self.ui.plainTextEdit.setPlaceholderText('Please enter information and set the timer')
+        if 'vaults.json' not in os.listdir():
+            with open('vaults.json', 'w') as file:
+                file.write(json.dumps({}))
         else:
-            self.ui.timeEdit.stepUp()
-            self.ui.floating_frame.setGraphicsEffect(self.blur)
-            self.set_timer()
+            with open('vaults.json', 'r') as file:
+                self.vaults = json.load(file)
+                if self.vaults == {}:
+                    print('No vaults')
+                else:
+                    self.compose_vaults()
+
+        self.ui.add_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
+        self.ui.back_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+        self.ui.save_btn.clicked.connect(lambda: self.add_new_vault())
 
 
-    def set_timer(self):
-        self.timer=QTimer()
-        self.timer.timeout.connect(self.progressbar_step)
-        self.timer.start(1000)
+    def compose_vaults(self):
+        for x, vault in enumerate(self.vaults):
+            self.delay = self.vaults[vault]
+            btn_name = f'button{x}'
 
-    def progressbar_step(self):
+            print(self.delay)
+            print(btn_name)
 
-        #self.ui.timeEdit.stepDown()
-        self.stepper += 1
-        if self.stepper%60 == 1:
-            self.ui.timeEdit.stepDown()
+            self.button = QPushButton(self.ui.scrollAreaWidgetContents)
+            self.button.setObjectName(btn_name)
+            self.button.setMinimumSize(QSize(0, 40))
+            self.button.setText(f'Vault {x+1}')
+            self.button.setStyleSheet(u"color: rgb(255, 255, 255);")
+            self.button.clicked.connect(lambda: self.start_timer(self.delay))
+            self.ui.verticalLayout_5.addWidget(self.button)
 
-        self.update_progress_bar(1 - self.init_value - self.step)
-        self.init_value += self.step
 
-        if self.init_value >= 1:
-            self.stop_and_show()
+    def start_timer(self, delay):
+        #self.ui.stackedWidget.setCurrentIndex(2)
+        print(delay)
+
+    def add_new_vault(self):
+        if self.ui.text_input.toPlainText() == '':
+            self.start_time = time.time()
+            self.ui.text_input.setPlaceholderText("This placholder can't be empty")
+            self.timer = QTimer()
+            self.timer.timeout.connect(
+                lambda: self.ui.text_input.setPlaceholderText("")
+                if (time.time() - self.start_time >= 1)
+                else None
+                )
             
-    def stop_and_show(self):
-        self.timer.stop()
-        self.ui.floating_frame.setGraphicsEffect(self.unblur)
-        
+            self.timer.start(1000)
 
-            
-    def update_progress_bar(self, value):
-        stop_1 = str(value)
-        stop_2 = str(value+0.001)
-        styleSheet = """
-                        border-radius: 150px;
-                        background-color: qconicalgradient(cx:0.5, cy:0.5, angle:90, 
-                        stop:{stop1} rgba(200, 33, 142, 0), 
-                        stop:{stop2} rgba(85, 170, 255, 255));"""
-
-
-        updateStyleSheet = styleSheet.replace("{stop1}", stop_1).replace("{stop2}", stop_2)
-        self.ui.circuralProgress.setStyleSheet(updateStyleSheet)
+        else:
+            pass
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
